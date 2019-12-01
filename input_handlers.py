473ad0e5f08495b3bcc98dff1d66e9093918b5a0
awheeler294 <awheeler294@gmail.com>
2019-12-01 08:06:50 -0700

    part 1: show a console, draw character, move character around screen

---
+++ input_handlers.py
@@ -0,0 +1,23 @@
+import tcod as libtcod
+
+def handle_keys(key):
+    # Movement keys
+    if key.vk == libtcod.KEY_UP:
+        return {'move': (0, -1)}
+    if key.vk == libtcod.KEY_DOWN:
+        return {'move': (0, 1)}
+    if key.vk == libtcod.KEY_LEFT:
+        return {'move': (-1, 0)}
+    if key.vk == libtcod.KEY_RIGHT:
+        return {'move': (1, 0)}
+
+    # Alt+Enter: toggle full screen
+    if  key.vk == libtcod.KEY_ENTER and key.lalt:
+        return {'fullscreen': True}
+
+    # Exit the game
+    elif key.vk == libtcod.KEY_ESCAPE:
+        return {'exit': True}
+
+    # No key was pressed
+    return {}
